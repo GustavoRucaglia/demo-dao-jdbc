fb53cb1295f3e6e8e545ef1b13e31050d4678f60
GustavoRucaglia <111381127+gustavo-Bozetti@users.noreply.github.com>
2023-08-08 13:43:59 -0300

    criando projeto

---
+++ src/db/DBIntegreteExcpetion.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DBIntegreteExcpetion extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DBIntegreteExcpetion(String msg) {
+		super(msg);
+	}
+
+}
