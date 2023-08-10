9661ef3c6511970722c187b00e56e7a4a8ec35fb
GustavoRucaglia <111381127+gustavo-Bozetti@users.noreply.github.com>
2023-08-10 19:41:15 -0300

    criando o deletebyID

--- src/appication/Program.java
+++ src/appication/Program.java
@@ -2,6 +2,7 @@ package appication;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1: Seller findByID ===");
 		
@@ -40,6 +42,11 @@ public class Program {
 		 seller = sellerDao.findById(1);
 		 seller.setName("Marta");
 		 sellerDao.update(seller);
+		 System.out.println("\n=== TEST 6: Seller findByDeparment ===");
+		 System.out.println("digite um id");
+		 int id = sc.nextInt();
+		 sellerDao.deleById(id);
+		 sc.close();
 	}
 
 }
